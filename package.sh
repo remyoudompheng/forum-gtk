c458f86671c82c3b74446590b0aaacdbfcf8f916
oudomphe <oudomphe>
2006-10-30 14:22:32 +0000

    Remove unused lines

--- package.sh
+++ package.sh
@@ -2,18 +2,18 @@
 
 rm -rf dist
 mkdir -p dist/bin
-mkdir -p dist/lib/forum-gtk
+#mkdir -p dist/lib/forum-gtk
 mkdir -p dist/share/forum-gtk
 mkdir -p dist/share/doc/forum-gtk
 
 DISTFILES="ChangeLog flrnkill.example flrnrc.example History"
 
 cp lib/*.py dist/share/forum-gtk
-for i in lib/*.py
-do
-    newi=`basename $i`
-    ln -s ../../share/forum-gtk/$newi dist/lib/forum-gtk/$newi
-done
+#for i in lib/*.py
+#do
+#    newi=`basename $i`
+#    ln -s ../../share/forum-gtk/$newi dist/lib/forum-gtk/$newi
+#done
 cp $DISTFILES dist/share/doc/forum-gtk
 
 cat > dist/bin/forum-gtk <<EOF
